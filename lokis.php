3f99a13818a498852eb89d3bf22fd298b80c34e6
June Katunge <june.katunge@strathmore.edu>
2018-04-13 21:23:14 +0300

    1st committ

---
+++ lokis.php
@@ -0,0 +1,26 @@
+<?php
+/*
+*print the following string in a new line
+*inplace of the spaces
+*@example
+*Wambua dreamt of cows
+*output
+*----Wambua
+*----dreaamt
+*---of
+*--cows
+*/
+
+
+
+
+
+
+$sentence="Wambua dreaamt of cows.";
+#solution 1
+//convert sentence to array
+$sentence_array=explode('',$sentence):
+//iterate the array printing at a time
+foreach
+ac
+?>
